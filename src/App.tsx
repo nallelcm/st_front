c6a9a2f0f823cfe8fb53e89fe391dacca294df1c
Mike McLellan <nallelcm@gmail.com>
2024-04-19 23:25:14 -0600

    fix: made it so it clears the context on logout (maybe?)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import { ThemeProvider } from "@mui/material";
 import { spaceTheme } from "./Themes";
 //import "./index.css";
 import ContentRouterLayout from "./components/ContentRouterLayout";
-
+import { useSpaceTrader } from "./contexts/SpaceTraderContext";
+import { useEffect } from "react";
+import { useAuth } from "./contexts/AuthProviderContext";
 function App() {
+  const { token } = useAuth();
+  const { clearData } = useSpaceTrader();
+  console.log("App");
+  useEffect(() => {
+    console.log("App useEffect");
+    if (!localStorage.getItem("token")) {
+      console.log("clearing data");
+      clearData();
+    }
+  }, [token]);
+
   return (
     <ThemeProvider theme={spaceTheme}>
       <ContentRouterLayout />
